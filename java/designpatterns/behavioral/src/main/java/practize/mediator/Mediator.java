5518f16065375165381b6b7665497d799abd6383
Sandip Singh <Sandip.Singh@altisource.com>
2017-03-19 23:54:14 +0530

    Added Mediator Design Pattern

---
+++ java/designpatterns/behavioral/src/main/java/practize/mediator/Mediator.java
@@ -0,0 +1,7 @@
+package practize.mediator;
+
+/**
+ * Created by sinsandi on 3/19/2017.
+ */
+public interface Mediator {
+}
---
+++ java/designpatterns/behavioral/src/test/java/practize/mediator/ChatRoomMediatorTest.java
@@ -0,0 +1,8 @@
+import static org.testng.Assert.*;
+
+/**
+ * Created by sinsandi on 3/19/2017.
+ */
+public class ChatRoomMediatorTest {
+
+}
